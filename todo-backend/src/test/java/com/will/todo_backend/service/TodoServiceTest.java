ca00adc97174552217438b2b003625af9841d5ff
WillBurdett <williamburdett1995@gmail.com>
2025-11-27 12:36:57 +0000

    further service tests added

--- todo-backend/src/test/java/com/will/todo_backend/service/TodoServiceTest.java
+++ todo-backend/src/test/java/com/will/todo_backend/service/TodoServiceTest.java
@@ -1,5 +1,6 @@
 package com.will.todo_backend.service;
 
+import com.will.todo_backend.model.api.TodoInput;
 import com.will.todo_backend.model.api.TodoOutput;
 import com.will.todo_backend.model.entity.TodoEntity;
 import com.will.todo_backend.model.enums.Defcon;
@@ -7,12 +8,13 @@ import com.will.todo_backend.repository.TodoRepo;
 import org.junit.jupiter.api.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -30,9 +32,10 @@ class TodoServiceTest {
     }
 
     @Nested
-    class get_all_todos {
+    class get_all_todos_should {
+
         @Test
-        void returns_all_todos() {
+        void return_all_todos_when_todos_exist() {
             // given
             List<TodoEntity> todos = List.of(
                     createTodoEntityWithTitle("task 1"),
@@ -49,14 +52,65 @@ class TodoServiceTest {
             assertEquals("task 2", result.get(1).getTitle());
             verify(todoRepo, times(1)).findAll();
         }
+
+        @Test
+        void return_an_empty_list_when_no_todos_exist() {
+            // given
+            List<TodoEntity> todos = List.of();
+            when(todoRepo.findAll()).thenReturn(todos);
+
+            // when
+            List<TodoOutput> result = undertest.getAllTodos();
+
+            // then
+            assertEquals(List.of(), result);
+            verify(todoRepo, times(1)).findAll();
+        }
     }
 
-    @Test
-    void createTodo() {
+    @Nested
+    class create_todo_should {
+
+        @Test
+        void create_todo_then_return_output() {
+            // given
+            TodoInput input = createTodoInputWithTitle("task 1");
+
+            // when
+            TodoOutput actualOutput = undertest.createTodo(input);
+
+            // then
+            TodoEntity expectedEntity = createTodoEntityWithTitle("task 1");
+            TodoOutput expectedOutput = createTodoOutputWithTitle("task 1");
+
+            verify(todoRepo).save(expectedEntity);
+            assertEquals(expectedOutput, actualOutput);
+        }
     }
 
-    @Test
-    void updateTodo() {
+    @Nested
+    class update_todo_should {
+
+        @BeforeEach
+        void setup() {
+            when(todoRepo.findById(1L)).thenReturn(Optional.of(createTodoEntityWithTitle("task 1")));
+        }
+
+        @Test
+        void update_todo_entity_then_return_updated_output() {
+            // given
+            TodoInput input = createTodoInputWithTitle("task 2");
+
+            // when
+            TodoOutput actualOutput = undertest.updateTodo(1L, input);
+
+            // then
+            TodoEntity expectedEntity = createTodoEntityWithTitle("task 2");
+            TodoOutput expectedOutput = createTodoOutputWithTitle("task 2");
+
+            verify(todoRepo).save(expectedEntity);
+            assertEquals(expectedOutput, actualOutput);
+        }
     }
 
     @Test
@@ -64,6 +118,14 @@ class TodoServiceTest {
     }
 
     private TodoEntity createTodoEntityWithTitle(String title) {
-        return new TodoEntity(title, "test entity", Defcon.ONE, null);
+        return new TodoEntity(title, "test todo", Defcon.ONE, null);
+    }
+
+    private TodoInput createTodoInputWithTitle(String title) {
+        return new TodoInput(title, "test todo", Defcon.ONE, null);
+    }
+
+    private TodoOutput createTodoOutputWithTitle(String title) {
+        return new TodoOutput(null, title, "test todo", Defcon.ONE, LocalDate.now(), null, false);
     }
 }
